95ac506868f81c967a8ca635007ef0cc38c41bb3
SesaiPK <sebastian.grudnik@student.pk.edu.pl>
2025-01-30 17:11:19 +0100

    add search bar feature

--- wiedmololbul/public/js/search.js
+++ wiedmololbul/public/js/search.js
@@ -1,48 +1,18 @@
-const search = document.querySelector('input[placeholder="Search..."]');
-const postContainer = document.querySelector(".posts");
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.querySelector('input[placeholder="Search..."]');
+    const searchForm = document.createElement("form");
 
-function loadPosts(posts) {
-    posts.forEach(post => {
-        console.log(post);
-        createPost(post);
+    searchForm.method = "GET";
+    searchForm.action = "/homepage";
+
+    // Umieść input w formularzu
+    searchInput.parentNode.replaceChild(searchForm, searchInput);
+    searchForm.appendChild(searchInput);
+
+    searchInput.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault(); // Zapobiega domyślnemu odświeżeniu strony
+            searchForm.submit(); // Wysyła formularz z frazą do Symfony
+        }
     });
-}
-
-function createPost(post) {
-    const template = document.querySelector("#post-template");
-
-    const clone = template.content.cloneNode(true);
-    const div = clone.querySelector("div");
-    div.id = post.id;
-    const title = clone.querySelector("h3");
-    title.innerHTML = post.title;
-    const author = clone.querySelector(".author");
-    author.innerHTML = `<strong>By: </strong>${post.author}`;
-    const content = clone.querySelector(".content");
-    content.innerHTML = post.content;
-
-
-    postContainer.appendChild(clone);
-}
-
-search.addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-
-        const data = {search: this.value};
-
-        fetch("/search", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }).then(function (response) {
-            return response.json();
-        }).then(function (posts) {
-            postContainer.innerHTML = "";
-            loadPosts(posts)
-        });
-    }
 });
-
--- wiedmololbul/src/Controller/HomepageController.php
+++ wiedmololbul/src/Controller/HomepageController.php
@@ -6,14 +6,21 @@ use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomepageController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function index(PostRepository $postRepository): Response
+    #[Route('/homepage', name: 'homepage')]
+    public function index(Request $request, PostRepository $postRepository): Response
     {
+        $query = $request->query->get('search'); // Pobranie wartości z searchbara
 
-        $posts = $postRepository->findAll();
+        if ($query) {
+            $posts = $postRepository->searchPosts($query); // Wyszukaj posty
+        } else {
+            $posts = $postRepository->findAllPosts();
+        }
+
 
         return $this->render('homepage.html.twig', [
             'posts' => $posts
