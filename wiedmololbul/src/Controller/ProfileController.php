e29cb7692407266f3612dca1a7179751ac26ed08
SesaiPK <sebastian.grudnik@student.pk.edu.pl>
2025-01-30 17:19:24 +0100

    create user profile page and add special features for admin

---
+++ wiedmololbul/src/Controller/ProfileController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class ProfileController extends AbstractController
+{
+    #[Route('/profile', name: 'app_profile')]
+    #[IsGranted("ROLE_USER")]
+    public function index(): Response
+    {
+        // Pobierz aktualnie zalogowanego użytkownika
+        $user = $this->getUser();
+
+        return $this->render('profile/index.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
